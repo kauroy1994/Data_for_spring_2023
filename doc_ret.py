624f4039a7cc066ceba399bc1f6f9524e698d549
Kaushik Roy <kaushikroy@MacBook-Pro-2.local>
2023-03-22 12:16:36 -0400

    added document retriever

---
+++ doc_ret.py
@@ -0,0 +1,12 @@
+from wiki_exps import get_info, get_all_subtopics_docs, get_topic_docs
+
+topics_of_interest = [' = Valkyria Chronicles III = ', ' = Tower Building of the Little Rock Arsenal = ']
+
+docs = get_info(info='main')
+main_texts = get_topic_docs(docs,topics_of_interest[0])
+
+sub_docs = get_info(info='sub')
+sub_texts = get_all_subtopics_docs(sub_docs,topics_of_interest[0])
+
+print (main_texts.keys())
+print (sub_texts.keys())
--- wiki_exps.py
+++ wiki_exps.py
@@ -1,3 +1,5 @@
+from tqdm import tqdm
+
 def get_info(info='main'):
 
     '''
@@ -30,12 +32,12 @@ def get_info(info='main'):
 
     if info == 'main':
 
-        topics = [] #will store the headings and sub-headings
         docs = {} #store the content under the headings and sub-headings
 
         topics = [line for line in file_lines if ' = ' in line and ' = = ' not in line]
         
-        for topic in topics: #for loop to store all the topic contents
+        print ('Extracting documents from the wiki dumps ...')
+        for topic in tqdm(topics): #for loop to store all the topic contents
             try:
                 topic_index = file_lines.index(topic)
                 next_topic_index = file_lines.index(topics[topic_index+1])
@@ -43,17 +45,71 @@ def get_info(info='main'):
             except:
                 continue
 
-        return topics, docs
+        return docs
 
     elif info == 'sub':
 
-        topics = [] #will store the headings and sub-headings
         docs = {} #store the content under the headings and sub-headings
 
         topics = [line for line in file_lines if ' = ' in line and ' = = ' not in line]
-
-        for topic in topics:
+        
+        print ('Extracting documents and sub documents from the wiki dumps ...')
+        for topic in tqdm(topics):
             try:
                 topic_index = file_lines.index(topic)
                 next_topic_index = file_lines.index(topics[topic_index+1])
-            
+                topic_content_lines = file_lines[topic_index+1:next_topic_index]
+
+                subtopics = [line for line in topic_content_lines if ' = = ' in line and ' = = = ' not in line]
+
+                for subtopic in subtopics:
+                    try:
+                        subtopic_index = file_lines.index(subtopic)
+                        next_subtopic_index = file_lines.index(subtopics[subtopic_index+1])
+                        docs[(topic,subtopic)] = ''.join(file_lines[subtopic_index+1:next_subtopic_index])
+                    except:
+                        continue
+            except:
+                continue
+        return docs
+
+    else:
+
+        print ('no executable function available for this script\nExiting program ...')
+        exit()
+
+def get_all_subtopics_docs(docs,topic):
+
+    '''
+    returns doc relevant to the topic and its subtopics
+    param-info:
+
+                docs = document dictionary returned by get_info(info='sub')
+                topic = a topic name e.g., ' = Tower Building of the Little Rock Arsenal = '
+    '''
+
+    topic_docs = {} #stores document contents
+    relevant_keys = [key for key in list(docs.keys()) if key[0] == topic]
+
+    for key in relevant_keys:
+        topic_docs[key] = docs[key]
+
+    return topic_docs
+
+def get_topic_docs(docs, topic):
+
+    '''
+    returns doc relevant to the topic
+    param-info:
+
+                docs = document dictionary returned by get_info(info='main')
+                topic = a topic name e.g., ' = Tower Building of the Little Rock Arsenal = '
+    '''
+
+    topic_docs = {} #stores document contents
+    relevant_keys = [key for key in list(docs.keys()) if key == topic]
+
+    for key in relevant_keys:
+        topic_docs[key] = docs[key]
+
+    return topic_docs
